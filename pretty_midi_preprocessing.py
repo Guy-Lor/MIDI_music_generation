01bb352eb2ece901cd8da46b9b833f68dee452cb
Guy-Lor <guyl@campus.technion.ac.il>
2021-09-04 20:02:58 +0300

    Implement time to note func

--- pretty_midi_preprocessing.py
+++ pretty_midi_preprocessing.py
@@ -4,8 +4,11 @@ import pretty_midi as pm
 import numpy as np
 import os
 
+# TODO Piano roll for drums!
+# TODO Add instruments join for piano roll (same instrument, same piano roll)
 # Sampling freq of the columns for piano roll. The higher, the more "timeline" columns we have.
-SAMPLING_FREQ = 2
+SAMPLING_FREQ = 5
+WINDOW_SIZE = 100
 
 
 def get_piano_roll(instrument, end_time):
@@ -15,28 +18,42 @@ def get_piano_roll(instrument, end_time):
 
 
 def print_instrument_info(i, instrument):
-    instrument_name = pm.program_to_instrument_name(instrument.program)
     instrument_notes_len = len(instrument.notes)
     end_time = instrument.get_end_time()
-    print(f"Instrument #{i} - {instrument_name} ({instrument.program}) | {instrument_notes_len} notes | {end_time:.2f} seconds")
+    print(f"Instrument #{i} - {instrument.name} ({instrument.program}) | {instrument_notes_len} notes | {end_time:.2f} seconds")
 
 
-def midi_preprocess(path='all_blues-Miles-Davis_dz.mid', instruments_dependencies=False, print_info=False, separate_midi_file=False):
+def get_time_note_dict(piano_roll):
+    times = np.unique(np.where(piano_roll > 0)[1])
+    index = np.where(piano_roll > 0)
+    dict_keys_time = {}
 
+    for time in times:
+        index_where = np.where(index[1] == time)
+        notes = index[0][index_where]
+        dict_keys_time[time] = notes
+
+    return dict_keys_time
+    # return list_of_dict_keys_time
+
+def get_train_target_lists(notes_list):
+    for i in range(-WINDOW_SIZE, len(notes_list)-WINDOW_SIZE,1):
+        pass
+
+
+def midi_preprocess(path, notes_hash, instruments_dependencies=False, print_info=False, separate_midi_file=False):
     instruments_piano_roll = {}
     midi_name = path.split('.', 1)[0].split("/")[-1]
     # Load MIDI file into PrettyMIDI object
     midi_data = pm.PrettyMIDI(path)
     end_time = midi_data.get_end_time()
-
     if print_info:
         print(f"\n---------- {midi_name} | {end_time:.2f} seconds -----------")
 
-    # Print all the instruments WITH is_drum flag
-    # print(midi_data.instruments)
-
-    # Separate tracks and gives info
+    # Separate tracks and print info
     for i, instrument in enumerate(midi_data.instruments):
+        # Fix instruments names
+        instrument.name = "Drums" if instrument.is_drum else pm.program_to_instrument_name(instrument.program)
 
         instruments_piano_roll[i] = get_piano_roll(instrument, end_time)
 
@@ -48,16 +65,53 @@ def midi_preprocess(path='all_blues-Miles-Davis_dz.mid', instruments_dependencie
             instrument_midi.instruments.append(instrument)
             instrument_midi.write(f'{midi_name}_{i}_instrument.mid')
 
-    # print(instruments_piano_roll)
+    ############################# Reminder from here on we only use 1 instrument !!! #############################################
+    dict_keys_time = get_time_note_dict(instruments_piano_roll[0])
+
+    for key in dict_keys_time.keys():
+        # print(str(dict_keys_time[key]))
+        dict_keys_time[key] = str(dict_keys_time[key])
+        notes_hash.add_new_note(dict_keys_time[key])
+
+    # total time of piano roll, not of the midi file in seconds
+    total_time = instruments_piano_roll[0].shape[1]
+    # print(instruments_piano_roll[0].shape)
+    notes_list = []
+    for time in range(0, total_time, 1):
+        if time not in dict_keys_time:
+            notes_list += [notes_hash.notes_dict['e']]
+        else:
+            current_note = dict_keys_time[time]
+            notes_list += [notes_hash.notes_dict[current_note]]
+
+    print(notes_list)
+
+
+class NotesHash:
+    def __init__(self):
+        self.notes_dict = {'e': 1}
+        self.reversed_notes_dict = {1: 'e'}
+        self.token_counter = 2
+
+    def add_new_note(self, new_note):
+        if new_note not in self.notes_dict.keys():
+            self.notes_dict[new_note] = self.token_counter
+            self.reversed_notes_dict[self.token_counter] = new_note
+            self.token_counter += 1
 
 
 def main():
     path = 'blues/'
+    path = 'classic_piano/'
     files = [i for i in os.listdir(path) if i.endswith(".mid")]
     print(files)
+    notes_hash = NotesHash()
+    # midi_preprocess(path='all_blues-Miles-Davis_dz.mid', notes_hash=notes_hash, instruments_dependencies=False, print_info=True, separate_midi_file=False)
+
     # reading each midi file
     for file in files:
-        midi_preprocess(path+file, instruments_dependencies=False, print_info=True, separate_midi_file=False)
+        midi_preprocess(path+file, notes_hash=notes_hash, instruments_dependencies=False, print_info=True, separate_midi_file=False)
+
 
 if __name__ == '__main__':
     main()
