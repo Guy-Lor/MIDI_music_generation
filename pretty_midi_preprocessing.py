fd477f8f7e391ba9884209d4d362ae86e2617a3a
Ran-Br <46054132+Ran-br@users.noreply.github.com>
2021-09-08 03:22:26 +0300

    Use one-hot encoded input

--- pretty_midi_preprocessing.py
+++ pretty_midi_preprocessing.py
@@ -218,14 +218,19 @@ class ModelTrainer:
 
     def preprocess_files(self):
         all_songs_real_notes = []
+        temp_all_songs_input_windows = []
         temp_all_songs_target_windows = []
 
         for file in self.files:
             real_notes_list, input_windows, target_windows = midi_preprocess(path=self.path + file, notes_hash=self.notes_hash,
                                                                              print_info=True, separate_midi_file=False)
-            self.all_songs_input_windows += [input_windows]
+            temp_all_songs_input_windows += [input_windows]
             temp_all_songs_target_windows += [target_windows]
             all_songs_real_notes += [real_notes_list]
+
+        for input_window in temp_all_songs_input_windows:
+            input_window = to_categorical(input_window, num_classes=self.notes_hash.get_size())
+            self.all_songs_input_windows += [input_window]
 
         for target in temp_all_songs_target_windows:
             target = to_categorical(target, num_classes=self.notes_hash.get_size())
@@ -239,7 +244,7 @@ class ModelTrainer:
         # down the network
         model = Sequential()
         # add LSTM layer
-        model.add(LSTM(self.batches, input_shape=(WINDOW_SIZE, 1)))
+        model.add(LSTM(self.batches, input_shape=(WINDOW_SIZE, self.notes_hash.get_size())))
         model.add(Dense(256))
         model.add(ReLU())
         model.add(Dense(256))
